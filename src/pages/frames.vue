2e2fda5f8cf1314ea4bfd1c21c8d8237d97b6843
zosky cube <no-reply@git-hub.com>
2025-10-03 14:02:17 -0400

    frames css improved

--- src/pages/frames.vue
+++ src/pages/frames.vue
@@ -19,9 +19,9 @@ const files = computed(() => {
 </script>
 
 <template>
-    <section class="grid grid-cols-12 mt-10 w-full gap-2 px-5">
-        <article v-for="id of files" :key="id">
-            <img :src="`/deathFrames/${id}.svg`" :alt="img" class="h-32 w-auto ring-1 rounded-t-xl ring-blue-900/50"/>
+    <section class="grid grid-cols-6 mt-10 w-full gap-2 px-5">
+        <article v-for="id of files" :key="id" class="flex flex-col h-full">
+            <img :src="`/deathFrames/${id}.svg`" :alt="img" class="mt-auto w-full h-auto ring-1 rounded-t-xl ring-blue-900/50"/>
             <div class="w-full text-center text-xs bg-blue-900/50">{{ steamNames?.[id] }}</div>
             <div class="rounded-b-xl font-bold w-full text-center text-xs bg-blue-900/50">{{ id }}</div>
         </article>
